aa74c07d1354b39ee257f19ef643b4c3830458b9
Ian Halliday <ianwjhalliday@gmail.com>
2019-02-03 18:30:53 -0800

    SideBar Navigation headers and dividers

--- src/containers/SideBar/SideBar.tsx
+++ src/containers/SideBar/SideBar.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Menu} from 'semantic-ui-react';
+import {Divider, Menu} from 'semantic-ui-react';
 import './SideBar.scss';
 import SideBarItem from "./SideBarItem/SideBarItem";
+import SideBarHeader from "./SideBarHeader/SideBarHeader";
 
 export class SideBar extends React.Component {
   render() {
@@ -10,13 +11,19 @@ export class SideBar extends React.Component {
         <SideBarItem highlight label="Home" icon="home" />
         <SideBarItem label="Trending" icon="fire" />
         <SideBarItem label="Followers" icon="spy" />
+        <Divider />
+        <SideBarHeader title="Library" />
         <SideBarItem label="History" icon="history" />
         <SideBarItem label="Watch later" icon="clock" />
         <SideBarItem label="Liked videos" icon="thumbs up" />
+        <Divider />
+        <SideBarHeader title="More from YouTube" />
         <SideBarItem label="Movies and Shows" icon="film" />
+        <Divider />
         <SideBarItem label="Report history" icon="flag" />
         <SideBarItem label="Help" icon="help circle" />
         <SideBarItem label="Send feedback" icon="comment" />
+        <Divider />
       </Menu>
     );
   }
---
+++ src/containers/SideBar/SideBarHeader/SideBarHeader.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Menu} from 'semantic-ui-react';
+import './SideBarHeader.scss';
+
+export interface SideBarHeaderProps {
+  title: string;
+}
+
+export function SideBarHeader(props: SideBarHeaderProps) {
+  const heading = props.title ? props.title.toUpperCase() : '';
+  return (
+    <Menu.Item>
+      <Menu.Header className="sidebar-header">{heading}</Menu.Header>
+    </Menu.Item>
+  );
+}
+
+export default SideBarHeader;
