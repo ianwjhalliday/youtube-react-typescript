b227632422488c62765066402d5d4e07e2678bf6
Ian Halliday <ianwjhalliday@gmail.com>
2019-02-03 19:11:34 -0800

    SideBar Navigation footer

--- src/containers/SideBar/SideBar.tsx
+++ src/containers/SideBar/SideBar.tsx
@@ -4,6 +4,7 @@ import './SideBar.scss';
 import SideBarItem from "./SideBarItem/SideBarItem";
 import SideBarHeader from "./SideBarHeader/SideBarHeader";
 import Subscriptions from "./Subscriptions/Subscriptions";
+import SideBarFooter from "./SideBarFooter/SideBarFooter";
 
 export class SideBar extends React.Component {
   render() {
@@ -26,6 +27,7 @@ export class SideBar extends React.Component {
         <SideBarItem label="Help" icon="help circle" />
         <SideBarItem label="Send feedback" icon="comment" />
         <Divider />
+        <SideBarFooter />
       </Menu>
     );
   }
