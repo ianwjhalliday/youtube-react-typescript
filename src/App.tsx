c9b5d35fd73beb7b6916661f55c97a77ba11f7d6
Ian Halliday <ianwjhalliday@gmail.com>
2019-02-10 12:32:50 -0800

    Watch skeleton and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Home} from './containers/Home/Home';
-import {AppLayout} from './components/AppLayout/AppLayout';
 import {Route, Switch} from 'react-router';
+import {AppLayout} from './components/AppLayout/AppLayout';
+import {Home} from './containers/Home/Home';
+import {Watch} from './containers/Watch/Watch';
 
 export const App: React.FC = () => {
   return (
     <AppLayout>
       <Switch>
+        <Route path="/watch" component={Watch}/>
         <Route path="/" component={Home}/>
       </Switch>
     </AppLayout>
---
+++ src/containers/Watch/Watch.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Watch.scss';
+
+export const Watch: React.FC = () => {
+  return (
+    <div>Watch component placeholder</div>
+  );
+};
