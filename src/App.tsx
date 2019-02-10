f434ce542c8d44a801a28802fa62dc4089e26152
Ian Halliday <ianwjhalliday@gmail.com>
2019-02-10 12:28:28 -0800

    Hook up react-router for / route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Home} from './containers/Home/Home';
 import {AppLayout} from './components/AppLayout/AppLayout';
+import {Route, Switch} from 'react-router';
 
 export const App: React.FC = () => {
   return (
     <AppLayout>
-      <Home/>
+      <Switch>
+        <Route path="/" component={Home}/>
+      </Switch>
     </AppLayout>
   );
 };
