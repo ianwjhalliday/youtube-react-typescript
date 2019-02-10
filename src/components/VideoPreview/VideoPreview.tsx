ed0d06350e88d9cabd889febf0de77b044704567
Ian Halliday <ianwjhalliday@gmail.com>
2019-02-09 19:26:18 -0800

    VideoPreview metadata

--- src/components/VideoPreview/VideoPreview.tsx
+++ src/components/VideoPreview/VideoPreview.tsx
@@ -12,6 +12,13 @@ export class VideoPreview extends React.Component {
             <span>05:22</span>
           </div>
         </div>
+        <div className="video-info">
+          <div className="semi-bold show-max-two-lines">Video title</div>
+          <div className="video-preview-metadata-container">
+            <div className="channel-title">Channel title</div>
+            <div><span>2.1M views • 2 days ago</span></div>
+          </div>
+        </div>
       </div>
     );
   }
