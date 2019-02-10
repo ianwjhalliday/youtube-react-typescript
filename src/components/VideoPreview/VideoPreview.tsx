463a015361a6b300a9a740b8d8613bb2622d6ade
Ian Halliday <ianwjhalliday@gmail.com>
2019-02-09 19:19:15 -0800

    VideoPreview layout

--- src/components/VideoPreview/VideoPreview.tsx
+++ src/components/VideoPreview/VideoPreview.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import {Image} from 'semantic-ui-react';
 import './VideoPreview.scss';
 
 export class VideoPreview extends React.Component {
   render() {
     return (
-      <div>This is a VideoPreview placeholder</div>
+      <div className="video-preview">
+        <div className="image-container">
+          <Image src="http://via.placeholder.com/210x118" />
+          <div className="time-label">
+            <span>05:22</span>
+          </div>
+        </div>
+      </div>
     );
   }
 }
